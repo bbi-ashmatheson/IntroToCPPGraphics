3fcea12d7ed7ff58ec16b557d6bda628766fdd02
Ash Matheson <nuclearfossil@hotmail.com>
2017-04-02 20:51:26 -0700

    Cleaning up the project generation script.
    Nothing to see here.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-IntroToCPPGraphics
+# Introduction to Graphics in C++
+
+## Summary
+
+This is a set of tutorials on Graphics programming. The initial main focus will be on DirectX (11, maybe 12).
+Subsequent tutorials will focus on Vulkan with a possible smattering of OpenGL.
+
+I'm doing this as more of an experiment, getting my Graphics coding up to a passible level once again,
+getting familiar with more 'modern' approaches to graphics programming as well as investigating other aspects
+of the genre (including GUI frameworks).
+
+## Technology used
+
+I'm a windows guy, so everything will be focused around the windows platform. That said, I'm not using a native
+Visual Studio solution. I'm using a new build system called 'Genie'.
+[link to main page](https://github.com/bkaradzic/GENie)
+[link to docs](https://github.com/bkaradzic/genie/blob/master/docs/scripting-reference.md#scripting-reference)
+
+## Building Instructions
+
+It's pretty straight forward. I'll add a batch script at some point in time, but it would do nothing more than:
+
+```
+cd code
+bin\genie.exe vs2015
+```
+
+You aren't limited to Visual Studio 2015, you can also use 2017 like so:
+```
+cd code
+bin\genie.exe vs2017
+```
+
+Why a build system? I use Visual Studio 2015 at work and 2017 Community at home. That's the simplest answer.
--- code/scripts/genie.lua
+++ code/scripts/genie.lua
@@ -1,36 +1,49 @@
+-- Solution definition
 solution "IntroToGraphicsCPP"
   location "../build"
 
+  -- Only Debug and Release configurations
   configurations {
     "Debug",
     "Release",
   }
 
+  -- Support for X86, X64 and 'native' (for other OSes)
   platforms {
     "x32",
     "x64",
     "Native",
   }
 
+  -- C++
   language "C++"
 
+-- Folder for the 'workspace'
 WORKSPACE_DIR = path.getabsolute("..")
-local THIRD_PARTY_DIR = path.join(BGFX_DIR, "3rdparty")
 
+-- For when we have a 3rd party library set (AssImp, any font libs, etc)
+local THIRD_PARTY_DIR = path.join(WORKSPACE_DIR, "3rdparty")
+
+-- Add in the toolchain.lua scrip and fire off the main script
+-- to set up the build environment
 dofile (path.join(WORKSPACE_DIR, "scripts/toolchain.lua"))
 if not toolchain(WORKSPACE_DIR, THIRD_PARTY_DIR) then
 	return -- no action specified
 end
 
+-- configureations for Debug
 configuration "Debug"
   defines { "WIN32", "_DEBUG", "_WINDOWS", "_UNICODE", "UNICODE", "%(PreprocessorDefinitions)" }
   links {"D3D11", "D3DCompiler"}
   links {"kernel32","user32","gdi32","winspool","comdlg32","advapi32","shell32","ole32","oleaut32","uuid","odbc32","odbccp32"}
+
+-- configuration for Release
 configuration "Release"
   defines { "WIN32", "NDEBUG", "_WINDOWS", "_UNICODE", "UNICODE", "%(PreprocessorDefinitions)" }
   links {"D3D11", "D3DCompiler"}
   links {"kernel32","user32","gdi32","winspool","comdlg32","advapi32","shell32","ole32","oleaut32","uuid","odbc32","odbccp32"}
 
+-- our first project
 project "intro01"
   PROJ_DIR = path.join(WORKSPACE_DIR, "intro01")
   flags { "WinMain"}
